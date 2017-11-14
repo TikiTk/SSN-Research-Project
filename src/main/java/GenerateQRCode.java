c8ffe9e2eacf892faea37a7a29fb60de4f7bb98a
TikiTk <mphahleletk@gmail.com>
2017-11-14 17:41:39 +0200

    Feature: Add dependencies and repository for ZXing

---
+++ src/main/java/GenerateQRCode.java
@@ -0,0 +1,17 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.glxn.qrgen.javase.QRCode;
+import net.glxn.qrgen.core.image.ImageType;
+
+
+public class GenerateQRCode {
+
+
+}
