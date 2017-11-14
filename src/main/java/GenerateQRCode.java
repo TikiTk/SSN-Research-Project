2fdae9faf5d312ea25270c971ae287aad10b57a2
TikiTk <mphahleletk@gmail.com>
2017-11-14 22:13:55 +0200

    refactor: remove absolute path and make it a variable that can be passed.

--- src/main/java/GenerateQRCode.java
+++ src/main/java/GenerateQRCode.java
@@ -8,17 +8,18 @@ import net.glxn.qrgen.core.image.ImageType;
 
 public class GenerateQRCode {
     public static void main(String[] args) {
-        generateQRimage("QR code test");
+        String path = "path to where file should be saved";
+        generateQRimage("QR code test", path);
 
     }
 
-    static void generateQRimage(String message) {
+    static void generateQRimage(String message, String path) {
         ByteArrayOutputStream byteStreamOfText = QRCode.from(message).withErrorCorrection(ErrorCorrectionLevel.H)
                 .to(ImageType.JPG).withSize(250, 250).stream();
 
         try {
             FileOutputStream imageFile = new FileOutputStream(new File(
-                    "C:\\Users\\Tk\\Documents\\GitHub\\QR.JPG"));
+                    path));
 
             imageFile.write(byteStreamOfText.toByteArray());
 
